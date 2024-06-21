d4b171ab14b9e18f3b989c8dcadfe8dc0d19ba7e
stoledcat <jetnavo@yandex.ru>
2024-06-21 15:41:52 +0300

    добавил выбор операции, начал оборачивать все в отдельные функции

--- code_selection.py
+++ code_selection.py
@@ -1,5 +1,18 @@
 input_file = open("input.txt", encoding="utf-8")
 
+choise = input('Что модифицировать?\n'
+               '1 - Коды блоков\n'
+               '2 - коды пачек\n'
+               'Выбор: ')
+
+
+# BUG Не отрабатывает запуск функции "bundle_code_modify"
+def run_choise(choise):
+    if choise == '1':
+        bundle_code_modify(input_file)
+    else:
+        print('Неверный выбор')
+
 
 def replace_quotes(new_string):
     out_string = ""
@@ -12,16 +25,19 @@ def replace_quotes(new_string):
     return out_string
 
 
-for string in input_file:
-    # Проверка на 52 и 53 символа идет потому что иногда при выгрузке кодов
-    # не ставится сивол переноса строки
-    if (len(string) >= 52 or len(string) <= 53) and string[0:5] == "01046":
-        new_line = string[0:35]
-        with open("output.txt", "a+", encoding="utf-8") as result:
-            print(f"'{replace_quotes(new_line)}',", file=result)
+# BUG не отрабатыает запуск этой функции
+def bundle_code_modify(input_file):
+    for string in input_file:
+        # Проверка на 52 и 53 символа идет потому что иногда при выгрузке кодов
+        # не ставится сивол переноса строки
+        if (len(string) >= 52 or len(string) <= 53) and string[0:5] == "01046":
+            new_line = string[0:35]
+            with open("output.txt", "a+", encoding="utf-8") as result:
+                print(f"'{replace_quotes(new_line)}',", file=result)
+    result.close()
 
 
 input_file.close()
-result.close()
+# result.close()
 
 # FIXME реализовать удаление запятой в последней строке кода
