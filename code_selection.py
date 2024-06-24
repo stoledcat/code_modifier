5f7c18320c3c58fcddab3f1b7b712e6aa6ed001e
stoledcat <jetnavo@yandex.ru>
2024-06-24 11:01:51 +0300

    release

--- code_selection.py
+++ code_selection.py
@@ -3,7 +3,7 @@
 
 input_file = open("input.txt", encoding="utf-8")
 
-#  Очистка либо создание файла для сохранения результатов выборки
+# Очистка либо создание файла для сохранения результатов выборки
 with open("output.txt", "w", encoding="utf-8") as output:
     output.close()
 
@@ -22,7 +22,7 @@ def replace_quotes_sql(new_line):
 
 # Экранирование кавычек для json
 def replace_quotes_json(new_line):
-    out_line = ''
+    out_line = ""
     for symbol in new_line:
         if symbol == '"':
             symbol = '\\"'
@@ -32,7 +32,7 @@ def replace_quotes_json(new_line):
     return out_line
 
 
-# CHECK # Модифицирование кодов пачек для SQL
+# Модифицирование кодов пачек для SQL
 def pack_code_sql_modify(input_file):
     count = 0
     for string in input_file:
@@ -47,7 +47,7 @@ def pack_code_sql_modify(input_file):
     result.close()
 
 
-# CHECK # Модифицирование кодов пачек для JSON
+# Модифицирование кодов пачек для JSON
 def pack_code_json_modify(input_file):
     count = 0
     for string in input_file:
@@ -62,7 +62,7 @@ def pack_code_json_modify(input_file):
     result.close()
 
 
-# CHECK Модифицирование кодов блоков для sql
+# Модифицирование кодов блоков для sql
 def bundle_code_sql_modify(input_file):
     count = 0
     for string in input_file:
@@ -77,7 +77,7 @@ def bundle_code_sql_modify(input_file):
     result.close()
 
 
-# CHECK # Модифицирование кодов блоков для json
+# Модифицирование кодов блоков для json
 def bundle_code_json_modify(input_file):
     count = 0
     for string in input_file:
@@ -86,13 +86,13 @@ def bundle_code_json_modify(input_file):
         if (len(string) == 52 or len(string) == 53) and string[0:5] == "01046":
             new_line = string[0:35]
             with open("output.txt", "a+", encoding="utf-8") as result:
-                print(f"'{replace_quotes_json(new_line)}',", file=result)
+                print(f'"{replace_quotes_json(new_line)}",', file=result)
             count += 1
     print(f"Операция выполнена. Изменено кодов блоков: {count}.")
     result.close()
 
 
-#  Точка входа, выбор типа модицикации
+# Точка входа, выбор типа модицикации
 def main():
     choise = ""
     code_choise = input(
@@ -101,15 +101,16 @@ def main():
         "2 - Коды блоков\n"
         "Выбор: "
     )
-    if code_choise != "1" or code_choise != "2":
-        print("Введено неверное значение, повторите ввод.")
-    format_choise = input("3 - Для SQL\n"
-                          "4 - Для JSON\n"
-                          "Выбор: ")
-    if format_choise != "3" or format_choise != "4":
-        print("Введено неверное значение, повторите ввод.")
+    format_choise = input(
+        "3 - Для SQL\n"
+        "4 - Для JSON\n"
+        "Выбор: "
+    )
 
     choise = code_choise + format_choise
+    if choise not in ("13", "14", "23", "24"):
+        print("Введено неверное значение, повторите ввод.")
+
     if choise == "13":
         pack_code_sql_modify(input_file)
     elif choise == "14":
