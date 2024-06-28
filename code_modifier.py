e8c38362b9f3d88c2dba808c3cb18cd095207b62
stoledcat <jetnavo@yandex.ru>
2024-06-28 14:37:45 +0300

    добавлена возможность скопировать результаты в буфер обмена

--- code_modifier.py
+++ code_modifier.py
@@ -182,7 +182,7 @@ def open_this():
     elif question == '2':
         with open("output.txt", 'r', encoding="utf-8") as result:
             clipboard = result.read()
-            pyperclip.copy(clipboard + '\n')
+            pyperclip.copy(clipboard)
     else:
         print('Неверный выбор')
 
