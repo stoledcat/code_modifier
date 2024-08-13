9b5c8d107b52e139436f709ec8fbf6c9112aaccf
stoledcat <jetnavo@yandex.ru>
2024-08-13 18:55:27 +0300

    добавлена функция поиска отсутствующих в коробе блоков

---
+++ find_missing_code.py
@@ -0,0 +1,20 @@
+def find_missing_code():
+    missing_codes = ""
+    file2 = open("duplicates2.txt", "r", encoding="utf-8").read()
+    with open("duplicates1.txt", "r", encoding="utf-8") as file1:
+        for string in file1:
+            new_string = string.replace('\\"', '"')
+            if new_string[35:42] not in file2:
+                string = string.strip()
+                missing_codes += f"{string.rstrip(',')}\n"
+        if missing_codes != "":
+            print(f"Отсутствующие блоки \n\
+(результат сохранен в файл 'missing_codes.txt'): \n{missing_codes}")
+        else:
+            print("Все блоки на месте.")
+    with open("missing_codes.txt", "w", encoding="utf-8") as outfile:
+        outfile.write(missing_codes)
+
+
+if __name__ == "__main__":
+    find_missing_code()
